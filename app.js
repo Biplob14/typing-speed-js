bfb054d77347399a0f8b88e3b9bb87a5d60e03c5
biplob14 <biploburrahaman@gmail.com>
2021-01-11 23:55:05 +0600

    updated js

---
+++ app.js
@@ -0,0 +1,5 @@
+const textWrap = document.querySelector('.textWrap');
+const testArea = document.querySelector('#text-area');
+const originText = document.querySelector('#origin-text p').innerHTML;
+const resetButton = document.querySelector('#reset');
+const theTimer = document.querySelector('.timer');
