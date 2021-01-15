104d179e7618cb01559dfbc79f83634567e30945
biplob14 <biploburrahaman@gmail.com>
2021-01-15 12:15:59 +0600

    necessary function created

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 const textWrap = document.querySelector('.textWrap');
-const testArea = document.querySelector('#text-area');
+const textArea = document.querySelector('#text-area');
 const originText = document.querySelector('#origin-text p').innerHTML;
 const resetButton = document.querySelector('#reset');
 const theTimer = document.querySelector('.timer');
+
+
+// spell check with comparison
+function spellCheck() {
+    let textEntered = textArea.value;
+
+    console.log(textEntered);
+}
+
+// timer activity
+function start() {
+   let textEnteredLength = textArea.value.length;
+   
+   console.log(textEnteredLength);
+}
+// Reset everything
+function reset() {
+    console.log('reset button pressed');
+}
+// event listeners for timer, spell check
+
+// detect keyboard stroke
+textArea.addEventListener('keypress', start, false);
+// compare spellings 
+textArea.addEventListener('keyup', spellCheck, false);
+// reset button
+resetButton.addEventListener('click', reset, false);
