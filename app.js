15120ccbda9635ccd09eb1c6570f76d9544583f3
biplob14 <biploburrahaman@gmail.com>
2021-01-19 13:03:58 +0600

    timer responding proper

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const textWrap = document.querySelector('.textWrap');
+const textWrapper = document.querySelector('.text-wrapper');
 const textArea = document.querySelector('#text-area');
 const originText = document.querySelector('#origin-text p').innerHTML;
 const resetButton = document.querySelector('#reset');
 const theTimer = document.querySelector('.timer');
+
 var timer = [0, 0, 0, 0];
+var interval;
+var timerRunning = false;
 
 // maintain two digits on timer output
 function leadingZero(time) {
@@ -15,7 +18,7 @@ function leadingZero(time) {
 // timer calculation
 function runTimer() {
     let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
-    theTimer.innerHTML = currentTime;
+    theTimer.innerHTML = "&#x23F1 " + currentTime;
     timer[3]++;
 
     timer[0] = Math.floor((timer[3]/100)/60);
@@ -25,22 +28,42 @@ function runTimer() {
 // spell check with comparison
 function spellCheck() {
     let textEntered = textArea.value;
-
+    let originTextMatch = originText.substring(0, textEntered.length);
     console.log(textEntered);
+    console.log(originTextMatch);
+    if(textEntered == originText) {
+        clearInterval(interval);
+        textWrapper.style.borderColor = "#008000";
+    } else {
+        if(textEntered == originTextMatch) {
+            textWrapper.style.borderColor = "#65CCF3";
+        } else
+            textWrapper.style.borderColor = "#E95D0f";
+    }
+
+    // console.log(textEntered);
 }
 
 // timer activity
 function start() {
    let textEnteredLength = textArea.value.length;
-   if(textEnteredLength === 0) {
-       setInterval(runTimer, 10);
+   if(textEnteredLength === 0 && !timerRunning) {
+       timerRunning = true;
+       interval = setInterval(runTimer, 10);
    }
    
-   console.log(textEnteredLength);
+//    console.log(textEnteredLength);
 }
 // Reset everything
 function reset() {
-    console.log('reset button pressed');
+    clearInterval(interval);
+    interval = null;
+    timer = [0, 0, 0, 0];
+    timerRunning = false;
+
+    textArea.value = "";
+    theTimer.innerHTML = "&#x23F1 00:00:00";
+    textWrapper.style.borderColor = "grey";
 }
 // event listeners for timer, spell check
 
